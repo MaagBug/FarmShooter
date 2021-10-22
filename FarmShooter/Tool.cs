3b23a83862643ff42cef03e6338ea37ee6615a0d
Lard Nixan <vmoyakkaunt@mail.ru>
2021-10-22 18:27:41 +0300

    Tools Created

---
+++ FarmShooter/Tool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace FarmShooter
+{
+    enum ToolType { Hoe, Axe, Pickaxe };
+
+    class Tool : Drawable
+    {
+        ToolType Type;
+
+        Sprite MainSprite;
+    }
+}
