655e8798bd40adcd97071e82cd2d8d705377b543
Lard Nixan <vmoyakkaunt@mail.ru>
2021-10-23 15:38:15 +0300

    Добавил инструменты, но теперь по-настоящему.

--- FarmShooter/Tool.cs
+++ FarmShooter/Tool.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using SFML.Graphics;
 using SFML.System;
+using SFML.Graphics;
 
 namespace FarmShooter
 {
     enum ToolType { Hoe, Axe, Pickaxe };
 
-    class Tool : Drawable
+    class Tool : Handheld
     {
-        ToolType Type;
+        public ToolType Type;
+        public float Efficiency;
 
-        Sprite MainSprite;
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+            target.Draw(MainSprite, states);
+        }
+
+        public override void Update()
+        {
+            MainSprite.Position = Owner.Handle.GetImagePoint(Owner.MainEntity.Sprite.Position, Owner.MainEntity.Sprite.Rotation);
+        }
+
+        public void Interact(InteractableResource res) 
+        {
+            if (res != null) 
+            {
+                res.Interact(this);
+            }
+        }
     }
 }
