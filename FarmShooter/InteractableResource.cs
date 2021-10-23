be9e9da8aff53d2c56b2969feae810c9df8c0dd7
Lard Nixan <vmoyakkaunt@mail.ru>
2021-10-23 15:37:52 +0300

    Добавил зачатки добываемых ресурсов.

---
+++ FarmShooter/InteractableResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace FarmShooter
+{
+    abstract class InteractableResource : Drawable
+    {
+        public Sprite MainSprite;
+        public int Durability;
+
+        public abstract void Draw(RenderTarget target, RenderStates states);
+
+        public abstract void Interact(Tool tool);
+    }
+}
