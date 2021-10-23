06240fdaf494eb221d4b5b4008516e6a81cf43ec
Lard Nixan <vmoyakkaunt@mail.ru>
2021-10-23 15:36:19 +0300

    Добавил фундамент для инструментов и оружия.

---
+++ FarmShooter/Handheld.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace FarmShooter
+{
+    abstract class Handheld : Drawable
+    {
+        public static event EventHandler<int> HandheldChanged;
+
+        public Sprite MainSprite;
+
+        public int ID 
+        {
+            get { return _ID; }
+            set 
+            {
+                _ID = value;
+                HandheldChanged?.Invoke(this, _ID);
+            }
+        }
+        private int _ID;
+
+        public Player Owner;
+
+        public abstract void Draw(RenderTarget target, RenderStates states);
+        public abstract void Update();
+    }
+}
