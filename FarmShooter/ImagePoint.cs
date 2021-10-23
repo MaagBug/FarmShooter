d8f298f2ac0aa2e56aed5cacd0822aa27508a7e7
Lard Nixan <vmoyakkaunt@mail.ru>
2021-10-23 15:37:33 +0300

    Добавил "Виртуальные точки" - особые координаты.

---
+++ FarmShooter/ImagePoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SFML.System;
+
+namespace FarmShooter
+{
+    class ImagePoint
+    {
+        public Vector2f Point;
+
+        public Vector2f GetImagePoint(Vector2f origin, float angle) 
+        {
+            return origin + new Vector2f((float)Math.Cos(Math.Atan2(Point.Y, Point.X) / (180 / 3.14) + angle / (180 / 3.14)),
+                                      (float)Math.Sin(Math.Atan2(Point.Y, Point.X) / (180 / 3.14) + angle / (180 / 3.14))) * (float)Point.Length();
+        }
+    }
+}
