b289c4772e21d65a331aafdc8bb3d15d88dc3ea7
Lard Nixan <vmoyakkaunt@mail.ru>
2021-10-28 21:30:06 +0300

    Добавил абстрактную вещь - её хранит инвентарь

---
+++ FarmShooter/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SFML.Graphics;
+
+namespace FarmShooter
+{
+    abstract class Item : Drawable
+    {
+        public Sprite InventorySprite;
+        public string Name;
+
+        public Item(string name, string inv_text_name) 
+        {
+            Name = name;
+            //InventorySprite = new Sprite(Program.Textures[inv_text_name]);
+        }
+
+        public abstract void Draw(RenderTarget target, RenderStates states);
+
+        public void Draw(RenderTexture texture, RenderStates states) 
+        {
+            texture.Draw(InventorySprite, states);
+        }
+    }
+}
