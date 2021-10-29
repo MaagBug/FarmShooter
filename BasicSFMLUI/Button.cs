3614e07e35420f9305de632f271813ffe2bfd732
Lard Nixan <vmoyakkaunt@mail.ru>
2021-10-29 17:03:46 +0300

    Добавил небольшую библиотеку объектов интерфейса

---
+++ BasicSFMLUI/Button.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace BasicSFMLUI
+{
+    public class Button
+    {
+        public event EventHandler Clicked;
+
+        public Sprite MainSprite;
+
+        public Texture MainTexture;
+        public Texture HoverTexture;
+        public Texture ClickTexture;
+
+        public Window ContainingWindow;
+
+        private bool is_being_pressed = false;
+
+        public Button(Texture main, Texture hover, Texture click) 
+        {
+            MainTexture = main;
+            HoverTexture = hover;
+            ClickTexture = click;
+
+            MainSprite = new Sprite(MainTexture);
+
+            ContainingWindow.MouseButtonPressed += (o, e) => 
+            {
+                if (MainSprite.GetGlobalBounds().Contains(e.X, e.Y)) is_being_pressed = true;
+                MainSprite.Texture = ClickTexture;
+            };
+
+            ContainingWindow.MouseButtonReleased += (o, e) => 
+            {
+                if (MainSprite.GetGlobalBounds().Contains(e.X, e.Y) && is_being_pressed) Clicked?.Invoke(this, new EventArgs());
+                MainSprite.Texture = MainTexture;
+                is_being_pressed = false;
+            };
+
+            ContainingWindow.MouseMoved += (o, e) => 
+            {
+                if (!is_being_pressed && MainSprite.GetGlobalBounds().Contains(e.X, e.Y)) MainSprite.Texture = MainTexture;
+            };
+        }
+    }
+}
---
+++ BasicSFMLUI/Canvas.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using SFML.Graphics;
+using SFML.System;
+
+namespace BasicSFMLUI
+{
+    public class Canvas : Drawable
+    {
+        public List<(Vector2f, Transformable)> Items = new List<(Vector2f, Transformable)>();
+        FloatRect Plane = new FloatRect(0, 0, 10, 10);
+
+        public void SetPlaneCenter(Vector2f center)
+        {
+            Plane.Left = center.X - Plane.Width / 2;
+            Plane.Top = center.Y - Plane.Height / 2;
+        }
+
+        public void SetPlaneSize(Vector2f size)
+        {
+            Plane.Width = size.X;
+            Plane.Height = size.Y;
+
+            foreach (var item in Items)
+            {
+                item.Item2.Position = new Vector2f(Plane.Width * item.Item1.X, Plane.Height * item.Item1.Y);
+            }
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            foreach (var item in Items)
+            {
+                if (item.Item2 is Drawable) target.Draw(item.Item2 as Drawable, states);
+            }
+        }
+    }
+}
