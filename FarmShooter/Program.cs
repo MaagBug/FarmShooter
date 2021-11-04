7ad49dca26f5f7a01a99068ba00f39a4cd1d7430
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-04 04:12:18 +0300

    Добавил меню и возможность добавлять больше сторонних ресурсов, а не только текстуры

--- FarmShooter/Program.cs
+++ FarmShooter/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using BasicSFMLUI;
+using System.Linq;
 
 namespace FarmShooter
 {
@@ -16,13 +17,14 @@ namespace FarmShooter
 
         public static List<Keyboard.Key> NumKeys = new List<Keyboard.Key> { Keyboard.Key.Num1, Keyboard.Key.Num2, Keyboard.Key.Num3, Keyboard.Key.Num4, Keyboard.Key.Num5, Keyboard.Key.Num6, Keyboard.Key.Num7, Keyboard.Key.Num8, Keyboard.Key.Num9, };
 
-        public static DTRenderWindow MainWindow = new DTRenderWindow(new VideoMode(800, 600), "test");
+        public static DTRenderWindow MainWindow = new DTRenderWindow(new VideoMode(1200, 800), "test");
         public static View MainView = new View(new FloatRect(0, 0, MainWindow.Size.X, MainWindow.Size.Y));
         public static View UIView = new View(new FloatRect(0, 0, MainWindow.Size.X, MainWindow.Size.Y));
 
         public static Canvas UICanvas = new Canvas();
 
         public static Dictionary<string, Texture> Textures = new Dictionary<string, Texture>();
+        public static Dictionary<string, object> OtherResources = new Dictionary<string, object>();
 
         public static Vector2f TrueMousePosition;
 
@@ -32,9 +34,10 @@ namespace FarmShooter
 
         public static float Zoom = 1;
 
-        static void LoadTextures() 
+        static void LoadTexturesAndResources() 
         {
-            string[] files = Directory.GetFiles("Textures");
+            List<string> files = Directory.GetFiles("Textures").ToList();
+            files.AddRange(Directory.GetFiles("Resources"));
 
             foreach (var file in files) 
             {
@@ -44,12 +47,16 @@ namespace FarmShooter
                 {
                     Textures.Add(Path.GetFileNameWithoutExtension(file), new Texture(file));
                 }
+                if (ext == ".ttf" || ext == ".otf") 
+                {
+                    OtherResources.Add(Path.GetFileNameWithoutExtension(file), new Font(file));
+                }
             }
         }
 
         static void LoadingScreen() 
         {
-            var load = Task.Run(LoadTextures);
+            var load = Task.Run(LoadTexturesAndResources);
 
             CircleShape load_shape = new CircleShape() { Radius = 30, Origin = new Vector2f(30, 30), Position = new Vector2f(400, 300), OutlineThickness = 5, OutlineColor = new Color(0, 0, 0) };
             load_shape.SetPointCount(4);
@@ -73,6 +80,74 @@ namespace FarmShooter
 
         static void Menu() 
         {
+            bool in_menu = true;
+
+            MainWindow.Closed += (o, e) => { MainWindow.Close(); };
+            MainWindow.MouseWheelScrolled += (o, e) =>
+            {
+                if ((e.Delta > 0 && Zoom < 2) || Zoom >= 2) Zoom += e.Delta;
+                MainView.Size = ((Vector2f)MainWindow.Size) * Zoom;
+            };
+            MainWindow.Resized += (o, e) =>
+            {
+                MainView.Size = new Vector2f(e.Width, e.Height) * Zoom;
+                UIView.Size = new Vector2f(e.Width, e.Height);
+                UIView.Center = new Vector2f(e.Width / 2, e.Height / 2);
+                UICanvas.SetPlaneSize(new Vector2f(e.Width, e.Height));
+            };
+
+            MainWindow.SetFramerateLimit(60);
+
+            Text GameTitle = new Text() { Font = (Font)OtherResources["NeutralFace-Bold"], DisplayedString = "[БЕЗ НАЗВАНИЯ]", CharacterSize = 60 };
+
+            Text PlayButtonText = new Text() { Font = (Font)OtherResources["NeutralFace-Bold"], DisplayedString = "ИГРАТЬ" };
+            Text ExitButtonText = new Text() { Font = (Font)OtherResources["NeutralFace-Bold"], DisplayedString = "ВЫЙТИ" };
+
+            Text MadeBy = new Text() { Font = (Font)OtherResources["NeutralFace-Bold"], DisplayedString = "СДЕЛАНО МА\'АГ БАГОМ", CharacterSize = 24};
+            MadeBy.Origin = new Vector2f(0, MadeBy.CharacterSize + 5);
+
+            UICanvas.Items.Add(new (new Vector2f(0.015F, 0.035F), GameTitle));
+            UICanvas.Items.Add(new (new Vector2f(0.035F, 0.3F), PlayButtonText));
+            UICanvas.Items.Add(new (new Vector2f(0.035F, 0.4F), ExitButtonText));
+            UICanvas.Items.Add(new (new Vector2f(0.015F, 1), MadeBy));
+
+            UICanvas.SetPlaneSize((Vector2f)MainWindow.Size);
+
+            Button PlayButton = new Button(MainWindow, PlayButtonText.GetGlobalBounds());
+            PlayButton.Clicked += (o, e) => { in_menu = false; };
+
+            Button ExitButton = new Button(MainWindow, ExitButtonText.GetGlobalBounds());
+            ExitButton.Clicked += (o, e) => { MainWindow.Close(); };
+
+            EventHandler<SizeEventArgs> RepositionButtonsOnResize = (o, e) =>
+            {
+                PlayButton.BoundingBox = PlayButtonText.GetGlobalBounds();
+                ExitButton.BoundingBox = ExitButtonText.GetGlobalBounds();
+            };
+
+            MainWindow.Resized += RepositionButtonsOnResize;
+
+            while (MainWindow.IsOpen && in_menu) 
+            {
+                MainWindow.DispatchEvents();
+                MainWindow.UpdateDeltaTime();
+
+                MainWindow.Clear();
+
+                MainWindow.SetView(UIView);
+
+                MainWindow.Draw(UICanvas);
+
+                MainWindow.Display();
+            }
+
+            UICanvas.Items.Remove(new(new Vector2f(0.015F, 0.035F), GameTitle));
+            UICanvas.Items.Remove(new(new Vector2f(0.035F, 0.3F), PlayButtonText));
+            UICanvas.Items.Remove(new(new Vector2f(0.035F, 0.4F), ExitButtonText));
+            UICanvas.Items.Remove(new(new Vector2f(0.015F, 1), MadeBy));
+
+            MainWindow.Resized -= RepositionButtonsOnResize;
+
             PrepareSinglePlayer();
         }
 
@@ -96,7 +171,24 @@ namespace FarmShooter
 
         static void MainGameScreen() 
         {
-            UICanvas.Items.Add(new(new Vector2f(0, 0), new RectangleShape(new Vector2f(100, 200)) { FillColor = new Color(255, 0, 0) }));
+            TransformableVertexArray UIInvenotyBar = new TransformableVertexArray();
+
+            UIInvenotyBar.Vertexes.Append(new Vertex(new Vector2f(0, 0), new Color(0, 0, 0, 0)));
+            UIInvenotyBar.Vertexes.Append(new Vertex(new Vector2f(890, 0), new Color(0, 0, 0, 0)));
+            UIInvenotyBar.Vertexes.Append(new Vertex(new Vector2f(890, 120), new Color(0, 0, 0, 255)));
+            UIInvenotyBar.Vertexes.Append(new Vertex(new Vector2f(0, 120), new Color(0, 0, 0, 255)));
+            UIInvenotyBar.Vertexes.PrimitiveType = PrimitiveType.Quads;
+
+            UIInvenotyBar.Origin = new Vector2f(445, 120);
+
+            UICanvas.Items.Add(new(new Vector2f(0.5F, 1), UIInvenotyBar));
+
+            List<RectangleShape> UIInventoryCells = new List<RectangleShape>()
+            {
+                new RectangleShape(new Vector2f(100, 100)) { FillColor = new Color(0, 0, 0, 0), OutlineColor = new Color(160, 160, 160), OutlineThickness = 7 }
+            };
+
+            UICanvas.SetPlaneSize((Vector2f)MainWindow.Size);
 
             Player = new Player() { MainEntity = new Entity(Textures["Player"]) { Speed = 500 }};
             Player.Start();
@@ -133,20 +225,6 @@ namespace FarmShooter
 
         static void Main()
         {
-            MainWindow.Closed += (o, e) => { MainWindow.Close(); };
-            MainWindow.MouseWheelScrolled += (o, e) =>
-            {
-                if ((e.Delta > 0 && Zoom < 2) || Zoom >= 2) Zoom += e.Delta;
-                MainView.Size = ((Vector2f)MainWindow.Size) * Zoom;
-            };
-            MainWindow.Resized += (o, e) => 
-            {
-                MainView.Size = new Vector2f(e.Width, e.Height) * Zoom;
-                UIView.Size = new Vector2f(e.Width, e.Height);
-                UIView.Center = new Vector2f(e.Width / 2, e.Height / 2);
-                UICanvas.SetPlaneSize(new Vector2f(e.Width, e.Height));
-            };
-
             LoadingScreen();
             Menu();
             MainGameScreen();
