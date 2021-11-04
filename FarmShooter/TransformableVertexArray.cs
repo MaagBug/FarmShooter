73461366d86f039bf84d951ff1468c899c3c3c3d
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-04 04:10:20 +0300

    Создал трансформируемый массив вершин, по большей части для интерфейса

---
+++ FarmShooter/TransformableVertexArray.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SFML.Graphics;
+using SFML.System;
+
+namespace FarmShooter
+{
+    class TransformableVertexArray : Transformable, Drawable
+    {
+        public VertexArray Vertexes = new();
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            states.Transform = Transform;
+            target.Draw(Vertexes, states);
+        }
+    }
+}
