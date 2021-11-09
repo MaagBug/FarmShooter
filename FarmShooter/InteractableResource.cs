032689649bbe5b638ba71d59848adacc5475fa67
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-09 18:46:52 +0300

    Доработал класс, добавил максимальную прочность и тип взаимодействующего инструмента

--- FarmShooter/InteractableResource.cs
+++ FarmShooter/InteractableResource.cs
@@ -2,8 +2,12 @@
 {
     abstract class InteractableResource : Drawable
     {
+        public int ID;
         public Sprite MainSprite;
         public int Durability;
+        public int MaxDurability;
+
+        public ToolType InteractToolType;
 
         public abstract void Draw(RenderTarget target, RenderStates states);
 
