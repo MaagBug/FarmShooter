7b44a1bd33c6f08c9ae0c4ed86d1d2bfa44dbcc1
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-09 18:48:08 +0300

    Добавил количество одинаковых предметов в предмете

--- FarmShooter/Item.cs
+++ FarmShooter/Item.cs
@@ -2,8 +2,16 @@
 {
     abstract class Item : Drawable
     {
+        public int ID;
         public Sprite InventorySprite;
         public string Name;
+
+        public int Quantity;
+
+        public Item(int ID) 
+        {
+            this.ID = ID;
+        }
 
         public Item(string name, string inv_text_name) 
         {
