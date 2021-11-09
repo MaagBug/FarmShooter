505ccceaf3b9111533cadaf32bd286f6901c4637
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-09 18:47:20 +0300

    Добавил конструктор копирования

--- FarmShooter/FarmCell.cs
+++ FarmShooter/FarmCell.cs
@@ -2,14 +2,22 @@
 {
     class FarmCell : Cell
     {
-        public FarmCell() : base(6)
+        public Plant PlantedPlant;
+
+        public FarmCell() : base(4)
         {
             
+        }
+
+        public void Plant(Plant plant) 
+        {
+            PlantedPlant = new Plant(plant);
         }
 
         public override void Draw(RenderTarget target, RenderStates states)
         {
             target.Draw(MainSprite, states);
+            if(PlantedPlant != null) target.Draw(PlantedPlant.MainSprite, states);
         }
     }
 }
