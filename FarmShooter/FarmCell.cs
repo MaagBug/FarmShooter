3761efb88cb28b4d88770647813dbc9e0f9a6849
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-09 17:58:35 +0300

    Начинаю добавлять клетки для выращивания чего-либо

---
+++ FarmShooter/FarmCell.cs
@@ -0,0 +1,15 @@
+﻿namespace FarmShooter
+{
+    class FarmCell : Cell
+    {
+        public FarmCell() : base(6)
+        {
+            
+        }
+
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+            target.Draw(MainSprite, states);
+        }
+    }
+}
