e231b1207fbe8591b57831ab2b190f22361ea897
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-14 22:36:35 +0300

    Доработал класс и добавил метод загрузки растений из файла (как у инструментов)

--- FarmShooter/Plant.cs
+++ FarmShooter/Plant.cs
@@ -2,26 +2,75 @@
 {
     internal class Plant : InteractableResource
     {
-        public int Stage = 0;
-        public readonly Item HarvestResource;
+        public static List<Plant> AllPlants = new List<Plant>();
 
-        public Plant(string text_name, int max_durability, ToolType type) 
+        public static void LoadPlants(string json) 
         {
-            MainSprite = new Sprite(Program.Textures[text_name]);
-            MainSprite.Origin = new Vector2f(MainSprite.Texture.Size.X / 2, MainSprite.Texture.Size.Y);
-            MaxDurability = max_durability;
-            Durability = max_durability;
-            InteractToolType = type;
+            DataTable table = JsonConvert.DeserializeObject<DataTable>(json);
+
+            for (int i = 0; i < table.Rows.Count; ++i) 
+            {
+                var row = table.Rows[i];
+
+                List<Texture> stages = new List<Texture>();
+                for (int t = 0; t < (int)(long)row.ItemArray[2]; ++t) 
+                {
+                    stages.Add(Program.Textures[(string)row.ItemArray[1] + "_Stage_" + t.ToString()]);
+                }
+
+                AllPlants.Add(new Plant((int)(long)row.ItemArray[0], stages, (int)(long)row.ItemArray[2], 100, (ToolType)(long)row.ItemArray[5], (string)row.ItemArray[4]));
+            }
         }
 
-        public Plant(Plant copy) 
+        public int Stage 
         {
-            ID = copy.ID;
-            MainSprite = new Sprite(copy.MainSprite);
-            HarvestResource = copy.HarvestResource;
-            MaxDurability = copy.MaxDurability;
-            Durability = copy.Durability;
-            InteractToolType = copy.InteractToolType;
+            get { return _Stage; }
+            set 
+            {
+                _Stage = value;
+                MainSprite.Texture = StagesTextures[value];
+            }
+        }
+
+        private int _Stage = 0;
+
+        public int MaxStage;
+        public readonly Item HarvestResource;
+
+        List<Texture> StagesTextures;
+
+        public Plant(int ID, List<Texture> stages, int max_stages, int max_durability, ToolType type, string name) 
+        {
+            this.ID = ID;
+
+            MainSprite = new Sprite(stages[0]);
+            MainSprite.Origin = new Vector2f(MainSprite.Texture.Size.X / 2, MainSprite.Texture.Size.Y);
+            Name = name;
+
+            MaxDurability = max_durability;
+            Durability = max_durability;
+
+            InteractToolType = type;
+
+            StagesTextures = stages;
+            MaxStage = max_stages;
+        }
+
+        public Plant(int ID) 
+        {
+            this.ID = ID;
+            MainSprite = new Sprite(AllPlants[ID].MainSprite);
+            HarvestResource = AllPlants[ID].HarvestResource;
+
+            Name = AllPlants[ID].Name;
+
+            MaxDurability = AllPlants[ID].MaxDurability;
+            Durability = AllPlants[ID].Durability;
+
+            InteractToolType = AllPlants[ID].InteractToolType;
+
+            StagesTextures = AllPlants[ID].StagesTextures;
+            MaxStage = AllPlants[ID].MaxStage;
         }
 
         public override void Draw(RenderTarget target, RenderStates states)
