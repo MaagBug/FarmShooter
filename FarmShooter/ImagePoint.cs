7fc2be49712639ed099458bb065313107ee61850
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-17 21:56:57 +0300

    Сделал значение угла точки задаваемым отдельно

--- FarmShooter/ImagePoint.cs
+++ FarmShooter/ImagePoint.cs
@@ -3,11 +3,12 @@
     class ImagePoint
     {
         public Vector2f Point;
+        public float Angle;
 
-        public Vector2f GetImagePoint(Vector2f origin, float angle) 
+        public Vector2f GetImagePoint(Vector2f origin) 
         {
-            return origin + new Vector2f(MathF.Cos(MathF.Atan2(Point.Y, Point.X) + angle / (180 / 3.14F)),
-                                         MathF.Sin(MathF.Atan2(Point.Y, Point.X) + angle / (180 / 3.14F))) * Point.Length();
+            return origin + new Vector2f(MathF.Cos(MathF.Atan2(Point.Y, Point.X) + Angle / (180 / 3.14F)),
+                                         MathF.Sin(MathF.Atan2(Point.Y, Point.X) + Angle / (180 / 3.14F))) * Point.Length();
         }
     }
 }
