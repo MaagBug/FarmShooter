bfbcda4770f422e6698ec07d931cb4d82d688662
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-17 21:36:53 +0300

    Удалил старый, закоменченный вариант работы функции класса

--- FarmShooter/ImagePoint.cs
+++ FarmShooter/ImagePoint.cs
@@ -6,9 +6,6 @@
 
         public Vector2f GetImagePoint(Vector2f origin, float angle) 
         {
-            //return origin + new Vector2f((float)Math.Cos(Math.Atan2(Point.Y, Point.X) * (180 / Math.PI)),
-            //                             (float)Math.Sin(Math.Atan2(Point.Y, Point.X) * (180 / Math.PI))) * Point.Length();
-
             return origin + new Vector2f(MathF.Cos(MathF.Atan2(Point.Y, Point.X) + angle / (180 / 3.14F)),
                                          MathF.Sin(MathF.Atan2(Point.Y, Point.X) + angle / (180 / 3.14F))) * Point.Length();
         }
