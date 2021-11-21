b0a8ca20239b3d4cc641064501e82fdf79822313
Lard Nixan <vmoyakkaunt@mail.ru>
2021-11-21 18:46:52 +0300

    Добавил метод расширения для словаря, позволяющее определить особое значение по умолчанию, если искомое значение не найдено

---
+++ FarmShooter/DictionaryExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace FarmShooter
+{
+    public static class DictionaryExtension
+    {
+        public static T GetValueOrSpecificDefault<TKey, T>(this Dictionary<TKey, T> dict, TKey key, T SD)
+            => dict.TryGetValue(key, out var value) ? value : SD;
+    }
+}
