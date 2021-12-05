2a86172215a22ce54ce23e5a30249312d37e9c0d
Lard Nixan <vmoyakkaunt@mail.ru>
2021-12-05 16:57:43 +0300

    Начинаю добавлять инвентарь как отдельный класс

---
+++ FarmShooter/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmShooter
+{
+    internal class Inventory
+    {
+        public Item this[int x, int y] 
+        {
+            get { return _Items[x, y]; }
+            set { }
+        }
+
+        private Item[,] _Items = new Item[3, 8];
+
+        public bool AddItem(Item item) { }
+
+        public bool GetItem(int item_id, out Item item) { }
+    }
+}
